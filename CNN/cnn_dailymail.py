b5a8552319a81c1bea70889de8c6fe5941eed798
jaishnu18 <135092308+jaishnu18@users.noreply.github.com>
2025-07-05 10:13:46 +0530

    Update cnn_dailymail.py

--- CNN/cnn_dailymail.py
+++ CNN/cnn_dailymail.py
@@ -13,9 +13,9 @@ data = sorted(data, key=lambda x: len(x["article"]))
 
 # === Models to evaluate ===
 models = {
-    #"llama2-7b": "meta-llama/Llama-2-7b-hf",
-    #"llama3.1-8b": "meta-llama/Meta-Llama-3-8B",
-    #"qwen2.5-7b": "Qwen/Qwen2.5-7B",
+    "llama2-7b": "meta-llama/Llama-2-7b-hf",
+    "llama3.1-8b": "meta-llama/Meta-Llama-3-8B",
+    "qwen2.5-7b": "Qwen/Qwen2.5-7B",
     "mistral-7b": "mistralai/Mistral-7B-v0.1"
 }
 
